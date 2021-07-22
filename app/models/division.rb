0093f0f22160590e8b76b93f3610018f614e5aec
@an280420 <angit280420@gmail.com>
2021-07-22 14:23:08 +0500

    add config for ancestory

--- app/models/division.rb
+++ app/models/division.rb
@@ -4,4 +4,5 @@ class Division < ApplicationRecord
   has_many :employment_terms
   has_many :divisions, class_name: "Division", foreign_key: "parent_id", dependent: :destroy
   belongs_to :parent, class_name: "Division", optional: true
+  has_ancestry ancestry_column: "parent_id"
 end
