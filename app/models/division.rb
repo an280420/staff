37a7a355540b4a855f802163ccfecbd89a4c8894
@an280420 <angit280420@gmail.com>
2021-07-26 22:30:39 +0500

    Add method for create json tree

--- app/models/division.rb
+++ app/models/division.rb
@@ -5,4 +5,22 @@ class Division < ApplicationRecord
   has_many :divisions, class_name: "Division", foreign_key: "parent_id", dependent: :destroy
   belongs_to :parent, class_name: "Division", optional: true
   acts_as_tree order: "name"
+
+  def tree_as_json
+    divisions = {name: name, children: children.map { |child| child.tree_as_json }}
+
+    divisions.as_json
+  end
+
+  # def self.tree_as_json
+  #   divisions ||= {}
+  #   all_divisions = self.all
+
+  #   all_divisions.each do |division|
+  #     divisions[division.id] = {name: division.name, children: division.children.tree_as_json }
+  #   end
+
+  #   divisions.as_json
+  # end
+
 end
